042db62b30c4dfcdb7ffb6279fc9f93f562105d4
Terashima <yamanogomora@gmail.com>
2019-08-10 19:33:27 +0900

    Add: #1 Codableプロトコルに準拠したオフィスとスペースの型をそれぞれ実装

---
+++ ReserveAppiOSClient/ResultDataSet.swift
@@ -0,0 +1,37 @@
+//
+//  OfficeData.swift
+//  ReserveAppiOSClient
+//
+//  Created by 寺島 洋平 on 2019/08/10.
+//  Copyright © 2019年 YoheiTerashima. All rights reserved.
+//
+
+import Foundation
+
+// オフィスのデータを表現する構造体
+struct Office: Codable {
+    var id: String
+    var name: String
+    var img: String
+    var spaceList: [Space]
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "officeId"
+        case name = "officename"
+        case img = "imgPath"
+        case spaceList = "spaceList"
+    }
+}
+
+// スペースのデータを表現する構造体
+struct Space: Codable {
+    var id: String
+    var name: String
+    var capacity: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case id = "spaceId"
+        case name = "spacename"
+        case capacity
+    }
+}
--- ReserveAppiOSClient/ViewController.swift
+++ ReserveAppiOSClient/ViewController.swift
@@ -9,10 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // オフィスの情報を格納する配列
+    var officeList = [Office]()
+    
+    let list = """
+    [
+        {
+            "officeId": "0001",
+            "officename": "オフィスA",
+            "imgPath": "hogehoge.jpg",
+            "spaceList": [
+                {
+                    "spaceId": "a1",
+                    "spacename": "会議室",
+                    "capacity": 20
+                },
+                {
+                    "spaceId": "a2",
+                    "spacename": "個室",
+                    "capacity": 1
+                },
+            ]
+        },
+        {
+            "officeId": "0002",
+            "officename": "オフィスB",
+            "imgPath": "fugafuge.jpg",
+            "spaceList": [
+                {
+                    "spaceId": "b1",
+                    "spacename": "ミーティングルーム",
+                    "capacity": 6
+                }
+            ]
+        }
+    ]
+    """.data(using: .utf8)!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        do {
+            let result = try JSONDecoder().decode([Office].self, from: list)
+            // リストに追加
+            self.officeList.append(contentsOf: result)
+            print("## success!\n\(officeList)")
+        } catch let error {
+            print("## error!\n\(error)")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
